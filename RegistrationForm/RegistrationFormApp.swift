538a2110598cd80b0d775314553ec20d8f69bb7d
David Goren <david.goren_ex@scotiabank.cl>
2021-05-03 16:30:50 -0400

    Initial Commit

---
+++ RegistrationForm/RegistrationFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegistrationFormApp.swift
+//  RegistrationForm
+//
+//  Created by usuario on 03-05-21.
+//
+
+import SwiftUI
+
+@main
+struct RegistrationFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
