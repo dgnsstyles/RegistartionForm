fed58a3d66f8e3b39345797ed806edfed66e3427
David Goren <david.goren_ex@scotiabank.cl>
2021-05-03 17:10:12 -0400

    primer commit y para que los chicos tengan

--- RegistrationForm/ContentView.swift
+++ RegistrationForm/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var firstName = ""
+    @State var lastName  = ""
+    @State var password = ""
+    @State var passwordAgain = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+        NavigationView{
+            VStack{
+                Form {
+                    Section {
+                        TextField("Primer Nombre", text: $firstName)
+                        TextField("Apellido", text: $lastName)
+                    }
+                    Section(footer: Text("Tu Contraseña debe tener minimo 8 caracteres")) {
+                        SecureField("Escribe tu Contraseña", text: $password)
+                        SecureField("Confirma tu contraseña", text: $passwordAgain)
+                        
+                    }
+                  
+                }
+                Button(action: {
+                    // hacer algo con la data del usuario...
+                }, label: {
+                    Text("Continuar")
+                        .frame(width: 250, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .background(Color.blue)
+                        .foregroundColor(Color.white)
+                        .cornerRadius(8)
+                }).padding()
+                
+            }.navigationTitle("Crear Cuenta")
+            
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
 }
